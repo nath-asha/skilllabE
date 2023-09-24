4a93de8db943d049395d9cbe144815768f3f43b8
nath-asha <omthasha@gmail.com>
2023-09-24 12:56:58 +0530

    co

--- site/src/App.js
+++ site/src/App.js
@@ -1,14 +1,30 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import MyRecipe from './MyRecipe';
+import AboutUs from './AboutUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-       
-        
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/myrecipe">My Recipe</Link>
+          </li>
+          <li>
+            <Link to="/aboutus">About Us</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/myrecipe" component={MyRecipe} />
+      <Route path="/aboutus" component={AboutUs} />
+    </Router>
   );
 }
 
