76acceb4eeb61b3413f7281aa5dd1880d7f7b80d
nath-asha <omthasha@gmail.com>
2023-09-24 12:54:52 +0530

    commit

---
+++ site/src/components/MyRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyRecipe = () => {
+  return (
+    <div>
+      <h1>My Recipe Page</h1>
+      {/* Display 6 food items here with images and prices */}
+    </div>
+  );
+};
+
+export default MyRecipe;
