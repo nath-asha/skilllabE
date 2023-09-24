f63adaff992cc8fc629ea7f626fe7d3ed2b0a2c1
nath-asha <omthasha@gmail.com>
2023-09-24 12:54:11 +0530

    commit

---
+++ site/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="image1.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        {/* Add more Carousel items as needed */}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Home;
